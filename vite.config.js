989a13d0cd79c8667707d6481b36524bfe60d0e7
pauloRzeszut <agustinrzeszut@hotmail.com>
2024-11-23 16:27:25 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Ensure this points to your project root
+  build: {
+    outDir: 'dist', // Ensure the output directory is properly defined
+  },
 })
